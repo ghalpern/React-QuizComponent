4b4790f96787f2eb60bbfd8ba249d99d2ce13e4b
Georgina Halpern <gehalper@microsoft.com>
2018-08-26 14:19:25 -0700

    second module complete

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,12 +6,14 @@ let quizData = require('./quiz_data.json');
 class Quiz extends Component {
     constructor(props) {
         super(props);
-        this.state = {quiz_position: 1};
+        this.state = {quiz_position: 1}
     }
     render() {
         return (
-            <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]} />
-        );
+            <div>
+                <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]} />
+            </div>
+        )
     }
 }
 
